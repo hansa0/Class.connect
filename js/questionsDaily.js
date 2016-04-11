41a032d31cc049f13ed1c2221205e226ddb561ff
Hansa Srinivasan <hansa@mit.edu>
2016-04-11 18:50:27 -0400

    Added reply box sort of

--- js/questionsDaily.js
+++ js/questionsDaily.js
@@ -1,5 +1,6 @@
-replyClick = function() {
-    console.log("REPLY BUTTON CLICKED")
+replyClick = function(e) {
+    console.log("REPLY BUTTON CLICKED", e)
+    $('<div id="replyDiv"><input id="reply" class="input" type="text" name="input" value=""></div>').insertAfter(e.target.parentNode)
 };
 
 $(document).ready(function() {
@@ -22,7 +23,7 @@ $(document).ready(function() {
         var button = document.createElement("button");        // Create a <button> element
         var buttonText = document.createTextNode("Reply");       // Create a text node
         button.appendChild(buttonText);
-        button.onclick=function(){replyClick()};
+        button.onclick=function(){replyClick(event)};
 
         qDiv.appendChild(button)   
 
