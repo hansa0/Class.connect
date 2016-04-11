38354b25247c1fbdfa6649978fac08a176327646
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-04-11 12:41:38 -0400

    Added libraries -- fullcalendar, jquery, moment

--- js/collections.js
+++ js/collections.js
@@ -5,3 +5,20 @@
 * 
 * 
 */
+
+assignments = [
+  {
+    assignment_name: "Assignment 1",
+    topic: 'Verbs',
+    file: 'N/A',
+    text: "Write ten sentences in present tense about your day.",
+    duedate: "2016-04-17"
+  },
+  {
+    assignment_name: "Assignment 2",
+    topic: 'Nouns',
+    file: 'N/A',
+    text: "Complete the homework sheet.",
+    duedate: "2016-04-20"
+  }
+]
