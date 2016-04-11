7273dfbe0d07f96a277d93322e9b166fade8249d
Danielle Penny <dpenny@mit.edu>
2016-04-10 23:14:14 -0400

    ugly

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ $(function() {
         newDiv.style.height="80px";
         newDiv.style.width="80px";
         newDiv.style.margin="20px";
+        newDiv.style.position="absolute";
+        newDiv.style.top="130px";
+        newDiv.style.left="120px";
         document.getElementById("left").appendChild(newDiv);
+        
         $(newDiv).attr('id', 'Nouns');
         $(newDiv).text("Nouns");
     
