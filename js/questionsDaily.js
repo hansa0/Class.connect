b4520202bd855ce840537384839e9f424f076549
Hansa Srinivasan <hansa@mit.edu>
2016-04-11 21:55:21 -0400

    Added reply functionality

--- js/questionsDaily.js
+++ js/questionsDaily.js
@@ -1,15 +1,14 @@
 replyClick = function(e) {
     console.log("REPLY BUTTON CLICKED", e)
-    // $('rightbody').append('<textarea id="TypeHere"> Type some text here.</textarea>' );
-    var textAreaHtml = '<div id="replyDiv"><textarea id="'+ e.target.parentNode.id+"_reply"+ '"">Type some text here.</textarea>';
+    var sp = ' onFocus="this.value=\'\'; this.onfocus=null;" ';
+    var textAreaHtml = '<div class="replyDiv"><textarea id="'+ e.target.parentNode.id+"_reply"+ '"'+sp+ ' ></textarea>';
     textAreaHtml = textAreaHtml + '<button type="button" id="'+ e.target.parentNode.id+'_submit' + '">Submit</button></div>';
-    $(textAreaHtml).insertAfter(e.target.parentNode)
+    // $(textAreaHtml).insertAfter(e.target.parentNode)
+    $(e.target.parentNode).append(textAreaHtml)
     var btn_id =e.target.parentNode.id+'_submit'; 
     var submit_button = document.getElementById(btn_id);
     submit_button.onclick=function(){replySubmit(event)};
 
-
-    // $('body').append('<script type="application/x-javascript">tinymce.init({selector:"#TypeHere"});</script>');
     initializeTinymce();
 };
 
@@ -22,8 +21,35 @@ initializeTinymce = function() {
 };
 
 replySubmit = function(e) {
-    console.log ("HADHFAHDSFHAS", e);
+    // console.log ("HADHFAHDSFHAS", e);
+    var textarea_id = e.target.parentNode.childNodes[1].id;
+    var text = tinymce.get(textarea_id).getContent();
+    var question_id = e.target.parentNode.parentNode.id + "_question"
+    // console.log("question_id ", question_id)
+    var question = document.getElementById(question_id).firstChild.nodeValue;
+    // console.log("QUestion: ", question)
+    var question_index = getQuestionIndex(question)
+    questions[question_index].reply = text;
+    questions[question_index].hasReply = true;
+    // console.log("questions: ", questions)
+
+
+    // console.log("TEXT AREA SAYS: ", text);
+
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+
+
 };
+
+getQuestionIndex = function(q) {
+    for (var i=0; i < questions.length; i++ ){
+        if (questions[i].question == q){
+            return i
+        }
+    }
+};
+
+
 
 $(document).ready(function() {
 
@@ -38,9 +64,12 @@ $(document).ready(function() {
         p_author.className = "questionAuthor"
         qDiv.appendChild(p_author)
 
+        var p_question = document.createElement("p")
         var question = document.createTextNode(questions[i].question);
-        question.className = "questionText"
-        qDiv.appendChild(question)
+        p_question.appendChild(question)
+        p_question.id = qDiv.id + "_question";
+        p_question.className = "questionText"
+        qDiv.appendChild(p_question)
 
         var button = document.createElement("button");        // Create a <button> element
         var buttonText = document.createTextNode("Reply");       // Create a text node
