8107a39260688f011a2508e12c0bff9e34d0fb55
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-04-11 20:41:08 -0400

    Renamed main.js to calendar.js, added selected_day variable, and updated calendar view such that days are clickable and takes user to the daily view.

--- js/calendar.js
+++ js/calendar.js
@@ -1,12 +1,15 @@
 
 
+
 $(document).ready(function() {
   // page is now ready
 
   // initialize the calendar
   // assignments is initialized in js/collections.js
+
   all_materials = []
   for (var i = 0; i < assignments.length; i++) {
+
     assignment = assignments[i];
     assignment_event = {
       title: assignment.assignment_name,
@@ -14,13 +17,23 @@ $(document).ready(function() {
     }
     all_materials.push(assignment_event);
   };
-    
+  console.log(all_materials);
+
+  
   $('#calendar').fullCalendar({
-      // put your options and callbacks here
-      // weekends: false,
-      events: all_materials
-  })
+    // put your options and callbacks here
+    // weekends: false,
+    
+
+    // includes all materials as events
+    events: all_materials, 
+
+    // goes to daily view for selected day
+    dayClick: function(date, jsEvent, view) {
+      selected_day = date.format();
+      var current_url = location.href;
+      location.href = current_url.replace("index", "daily");
+    }
+
+  });
 });
-
-
-
