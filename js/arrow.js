174b71ff0444546aee27934787c93c6bb93355b8
Quoc-Van Tran <Q-Tran@users.noreply.github.com>
2016-04-13 15:20:26 -0400

    Create arrow.js

---
+++ js/arrow.js
@@ -0,0 +1,14 @@
+$(function() {
+    $( ".left-arrow").button({
+        icons: {
+            primary: "ui-icon-carat-1-w"
+        },
+        text: false
+    });
+    $( ".right-arrow" ).button({
+        icons: {
+            primary: "ui-icon-carat-1-e"
+        },
+        text: false
+    });
+});
