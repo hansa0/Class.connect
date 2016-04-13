4dc5dc72dc4737a5c9086971d21ee1eb7a8afa6f
Hansa Srinivasan <hansa@mit.edu>
2016-04-13 15:22:00 -0400

    changed more questions page styling

--- js/questions.js
+++ js/questions.js
@@ -29,7 +29,6 @@ replyClick = function(e) {
     
     
     if (questions[question_index].hasReply) {
-        console.log("HAS A FUCKING REPLY HANSA BREATHE GODDMANIT")
         var textarea_id = "#"+e.target.parentNode.id+"_reply"+ g
         console.log(textarea_id)
         $(textarea_id).text(questions[question_index].reply)
@@ -53,6 +52,7 @@ replySubmit = function(e) {
     var textarea_id = e.target.parentNode.childNodes[1].id;
     var text = tinymce.get(textarea_id).getContent();
     var question_id = e.target.parentNode.parentNode.id + "_question"
+    var description_id = e.target.parentNode.parentNode.id + "_description"
     // console.log("question_id ", question_id)
     var question = document.getElementById(question_id).firstChild.nodeValue;
     // console.log("QUestion: ", question)
@@ -67,8 +67,8 @@ replySubmit = function(e) {
         questions[question_index].hasReply = true;
         // console.log("questions: ", questions)
 
-        var reply_html = '<p id="'+ e.target.parentNode.parentNode.id + '_replyText' +'" class="replyText">'+text+'</p>'
-        $(reply_html).insertAfter(document.getElementById(question_id))
+        var reply_html = '<hr style ="border-top: 1px solid #D6DBDF " > <p id="'+ e.target.parentNode.parentNode.id + '_replyText' +'" class="replyText">'+text+'</p>'
+        $(reply_html).insertAfter(document.getElementById(description_id))
     }
 
     var replybtn_id = "#"+e.target.parentNode.parentNode.id + "_replybtn"
@@ -117,6 +117,7 @@ $(document).ready(function() {
 
 
         var description = document.createElement("p");
+        description.id = qDiv.id + "_description";
         var question_description = questions[i].question_description;
         $(description).append(question_description);
         $(qDiv).append(description);
